2b8fbc83cab7b8f6a834f2998058ee1cf002cfaf
MAD-2 <mmuradiabdullah88@gmail.com>
2021-11-27 16:26:05 +0300

    ..

--- js/app.js
+++ js/app.js
@@ -94,23 +94,6 @@ srar.style.display="inline"
 }
 
 
-// setting rates based on moves
-    if (moves > 8 && moves < 12){
-        for( i= 0; i < 3; i++){
-            if(i > 1){
-                stars[i].style.visibility = "collapse";
-            }
-        }
-    }
-    else if (moves > 13){
-        for( i= 0; i < 3; i++){
-            if(i > 0){
-                stars[i].style.visibility = "collapse";
-            }
-        }
-    }
- 
-
 
 //timer==========
 
